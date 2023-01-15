c73d18c023c568fae506c26516a39f19583a18af
Mike Robles <mikej.robles@gmail.com>
2023-01-15 14:36:07 -0500

    creates review schema

--- models/anime.js
+++ models/anime.js
@@ -2,12 +2,20 @@ import mongoose, { Schema } from 'mongoose'
 
 const schema = mongoose.Schema
 
+const reviewScehma = new Schema({
+  content: String,
+  rating: {type: Number, min: 1, max: 10, default: 10}
+}, {
+  timestamps: true
+})
+
 const animeSchema = new Schema({
   title: String,
   releaseYear: {type: Number, default: 2010},
   englishReleaseYear: {type: Number, default: 2010},
   cast: [String],
   englishDubCast: [String], 
+  reviews: [reviewSchema]
 }, {
   timestamps: true
 })
